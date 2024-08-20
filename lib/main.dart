fbfe0cd19feb85b92f7d12f8c780ae29ba250a44
dariolggomez <dariogomez1011@gmail.com>
2024-08-20 15:37:30 -0400

    create start screen widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/start_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,55 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Colors.blue, Colors.purple],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter)),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/quiz-logo.png',
-                  width: 250,
-                  height: 250,
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  'Learn Flutter the fun way!',
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.purple,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 30,
-                      vertical: 15,
-                    ),
-                    textStyle: const TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  child: const Text('Start Quiz'),
-                ),
-              ],
-            ),
-          ),
-        ),
+        body: StartScreen(),
       ),
     );
   }
---
+++ lib/start_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.blue, Colors.purple],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter)),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 250,
+              height: 250,
+              color: const Color.fromARGB(117, 255, 255, 255),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.purple,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 15,
+                ),
+                textStyle: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              label: const Text('Start Quiz'),
+              icon: const Icon(Icons.arrow_right_alt),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
