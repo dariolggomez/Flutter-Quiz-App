82b00da0e8e9e75a69c7b9b4b865ed53d6bbd427
dariolggomez <dariogomez1011@gmail.com>
2024-08-20 16:00:29 -0400

    convert quizz app widget into a stateful widget

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/start_screen.dart';
 
-class QuizApp extends StatelessWidget {
+class QuizApp extends StatefulWidget {
   const QuizApp({super.key});
 
+  @override
+  _QuizAppState createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
