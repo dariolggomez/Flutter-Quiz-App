f4c7ff623f68d751e3d1219369ae616207b8ee44
dariolggomez <dariogomez1011@gmail.com>
2024-08-22 14:37:10 -0400

    create the method and the state for switching widgets

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/questions_screen.dart';
 import 'package:flutter_quiz_app/start_screen.dart';
 
 class QuizApp extends StatefulWidget {
   const QuizApp({super.key});
 
   @override
-  _QuizAppState createState() => _QuizAppState();
+  State createState() => _QuizAppState();
 }
 
 class _QuizAppState extends State<QuizApp> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen(Widget screen) {
+    setState(() {
+      activeScreen = QuestionsScreen();
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,7 +30,7 @@ class _QuizAppState extends State<QuizApp> {
                   colors: [Colors.blue, Colors.purple],
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter)),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
