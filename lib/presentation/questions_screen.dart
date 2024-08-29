6af5ec66060871f8adb614dbe22905c6ee8d6911
dariolggomez <dariogomez1011@gmail.com>
2024-08-29 16:48:04 -0400

    import and use the questions text in the questions screen

--- lib/presentation/questions_screen.dart
+++ lib/presentation/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/widgets/buttons/elevated_button_answer.dart';
+import 'package:flutter_quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,15 +10,16 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final firstQuestion = questions[0];
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            "Question 1",
-            style: TextStyle(
+          Text(
+            firstQuestion.question,
+            style: const TextStyle(
               fontSize: 25,
               color: Colors.white,
             ),
@@ -25,15 +27,19 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           const SizedBox(
             height: 50,
           ),
-          ElevatedButtonAnswer(text: "Button1", onPressed: () {}),
+          ElevatedButtonAnswer(text: firstQuestion.answers[0], onPressed: () {}),
           const SizedBox(
             height: 20,
           ),
-          ElevatedButtonAnswer(text: "Button2", onPressed: () {}),
+          ElevatedButtonAnswer(text: firstQuestion.answers[1], onPressed: () {}),
           const SizedBox(
             height: 20,
           ),
-          ElevatedButtonAnswer(text: "Button3", onPressed: () {}),
+          ElevatedButtonAnswer(text: firstQuestion.answers[2], onPressed: () {}),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButtonAnswer(text: firstQuestion.answers[2], onPressed: () {}),
         ],
       ),
     );
